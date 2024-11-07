7b4744c7f87846e567b6bcb864bc08e136d0521f
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-11-07 20:06:05 +0800

    Add grid for chessboard

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
+use bevy_ecs_tilemap::prelude::*;
 
 fn main() {
     let mut app = App::new();
@@ -10,6 +11,7 @@ fn main() {
             // Prevent anti-aliasing
             .set(ImagePlugin::default_nearest()),
     )
+    .add_plugins(TilemapPlugin)
     .add_systems(Startup, setup)
     .add_systems(Update, update_cursor)
     .run();
@@ -22,6 +24,45 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn(Camera2dBundle::default());
+
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
+    let map_size = TilemapSize { x: 8, y: 8 };
+    let tilemap_entity = commands.spawn_empty().id();
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    // Create a checkerboard pattern by selecting the light or dark tile depending
+                    // on whether the sum of its coordinates is even or odd.
+                    texture_index: TileTextureIndex((x + y) % 2),
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 10.0, y: 10.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0)
+            .with_scale(Vec3::splat(10.0)),
+        ..Default::default()
+    });
+
     commands.spawn(MaterialMesh2dBundle {
         mesh: Mesh2dHandle(meshes.add(Rectangle::new(50.0, 50.0))),
         material: materials.add(Color::linear_rgb(1.0, 1.0, 1.0)),
