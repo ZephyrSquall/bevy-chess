26cb169197825d2fe3c738e9ceaa54189a1acd0d
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-11-08 22:46:40 +0800

    Place pieces in starting position

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,45 @@ const SCALED_GRID_SIZE: TilemapGridSize = TilemapGridSize {
     y: GRID_SIZE.y * SCALE,
 };
 
+enum Piece {
+    Bishop,
+    King,
+    Knight,
+    Pawn,
+    Queen,
+    Rook,
+}
+
+enum Color {
+    White,
+    Black,
+}
+
+#[derive(Component)]
+struct GamePiece {
+    piece: Piece,
+    color: Color,
+}
+
+impl GamePiece {
+    fn get_asset_path(&self) -> &str {
+        match (&self.piece, &self.color) {
+            (Piece::Bishop, Color::White) => "pieces/bishop_white.png",
+            (Piece::King, Color::White) => "pieces/king_white.png",
+            (Piece::Knight, Color::White) => "pieces/knight_white.png",
+            (Piece::Pawn, Color::White) => "pieces/pawn_white.png",
+            (Piece::Queen, Color::White) => "pieces/queen_white.png",
+            (Piece::Rook, Color::White) => "pieces/rook_white.png",
+            (Piece::Bishop, Color::Black) => "pieces/bishop_black.png",
+            (Piece::King, Color::Black) => "pieces/king_black.png",
+            (Piece::Knight, Color::Black) => "pieces/knight_black.png",
+            (Piece::Pawn, Color::Black) => "pieces/pawn_black.png",
+            (Piece::Queen, Color::Black) => "pieces/queen_black.png",
+            (Piece::Rook, Color::Black) => "pieces/rook_black.png",
+        }
+    }
+}
+
 fn main() {
     let mut app = App::new();
     app.add_plugins(
@@ -65,33 +104,371 @@ fn setup_board(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn setup_pieces(
     mut commands: Commands,
-    tile_q: Query<(Entity, &TilePos)>,
-    tilemap_transform_q: Query<&Transform, With<TileStorage>>,
+    tilemap_q: Query<(&Transform, &TileStorage)>,
     asset_server: Res<AssetServer>,
 ) {
-    // Get the offset of the board from the center
     let mut tilemat_offset = Vec2 { x: 0.0, y: 0.0 };
-    for transform in &tilemap_transform_q {
+    for (transform, tile_storage) in &tilemap_q {
+        // Get the offset of the board from the center
         tilemat_offset.x = transform.translation.x;
         tilemat_offset.y = transform.translation.y;
-    }
 
-    for (tile_id, tile_pos) in &tile_q {
-        let center = tile_pos.center_in_world(&SCALED_GRID_SIZE, &MAP_TYPE) + tilemat_offset;
-
-        commands.entity(tile_id).insert(SpriteBundle {
-            texture: asset_server.load("pieces/king_white.png"),
-            transform: Transform {
-                translation: Vec3 {
-                    x: center.x,
-                    y: center.y,
-                    z: 1.0,
-                },
-                scale: Vec3::splat(SCALE),
-                ..default()
+        // Place the starting pieces on the board.
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Rook,
+                color: Color::White,
             },
-            ..default()
-        });
+            TilePos { x: 0, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Knight,
+                color: Color::White,
+            },
+            TilePos { x: 1, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Bishop,
+                color: Color::White,
+            },
+            TilePos { x: 2, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Queen,
+                color: Color::White,
+            },
+            TilePos { x: 3, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::King,
+                color: Color::White,
+            },
+            TilePos { x: 4, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Bishop,
+                color: Color::White,
+            },
+            TilePos { x: 5, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Knight,
+                color: Color::White,
+            },
+            TilePos { x: 6, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Rook,
+                color: Color::White,
+            },
+            TilePos { x: 7, y: 0 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 0, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 1, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 2, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 3, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 4, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 5, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 6, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::White,
+            },
+            TilePos { x: 7, y: 1 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 0, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 1, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 2, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 3, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 4, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 5, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 6, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Pawn,
+                color: Color::Black,
+            },
+            TilePos { x: 7, y: 6 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Rook,
+                color: Color::Black,
+            },
+            TilePos { x: 0, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Knight,
+                color: Color::Black,
+            },
+            TilePos { x: 1, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Bishop,
+                color: Color::Black,
+            },
+            TilePos { x: 2, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Queen,
+                color: Color::Black,
+            },
+            TilePos { x: 3, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::King,
+                color: Color::Black,
+            },
+            TilePos { x: 4, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Bishop,
+                color: Color::Black,
+            },
+            TilePos { x: 5, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Knight,
+                color: Color::Black,
+            },
+            TilePos { x: 6, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
+        insert_piece_at_position(
+            GamePiece {
+                piece: Piece::Rook,
+                color: Color::Black,
+            },
+            TilePos { x: 7, y: 7 },
+            &mut commands,
+            tile_storage,
+            tilemat_offset,
+            &asset_server,
+        );
     }
 }
 
@@ -125,4 +502,36 @@ fn update_cursor(
             });
         }
     }
+}
+
+fn insert_piece_at_position(
+    game_piece: GamePiece,
+    tile_pos: TilePos,
+    commands: &mut Commands,
+    tile_storage: &TileStorage,
+    tilemat_offset: Vec2,
+    asset_server: &Res<AssetServer>,
+) {
+    // Must call to_string() to create an owned copy of the string, as the &str lifetime doesn't
+    // last long enough.
+    let asset_path = game_piece.get_asset_path().to_string();
+    let center = tile_pos.center_in_world(&SCALED_GRID_SIZE, &MAP_TYPE) + tilemat_offset;
+
+    let tile_id = tile_storage
+        .get(&tile_pos)
+        .expect("All board positions should have a tile entity");
+
+    commands.entity(tile_id).insert(SpriteBundle {
+        texture: asset_server.load(asset_path),
+        transform: Transform {
+            translation: Vec3 {
+                x: center.x,
+                y: center.y,
+                z: 1.0,
+            },
+            scale: Vec3::splat(SCALE),
+            ..default()
+        },
+        ..default()
+    });
 }
